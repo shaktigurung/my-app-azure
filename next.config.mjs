be638d052d00699fe815822b3b82266d93e37989
Shakti Gurung <Shakti.Gurung@optus.com.au>
2025-02-21 11:29:43 +1000

    configuration for deployment to azure

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "standalone",
+    reactStrictMode: true,
+    distDir: 'build',
+    output: 'standalone',
 };
 
 export default nextConfig;
